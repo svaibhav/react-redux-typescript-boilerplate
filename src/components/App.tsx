fc43450422e92b25ba261e7599444e1771975212
Swapnil Vaibhav <svaibhav@microsoft.com>
2018-10-07 11:43:28 +0530

    linting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,20 @@
 import * as React from "react";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Footer from "./common/footer/Footer";
 import Header from "./common/header/Header";
 import MyRoutes from "../routes/MyRoutes";
-import { BrowserRouter } from "react-router-dom";
 
 class App extends React.Component {
   public render(): JSX.Element {
     return (
       <div className="App">
         <Header appTitle="Welcome to React.js" />
+
         <BrowserRouter>
           <MyRoutes />
         </BrowserRouter>
+
         <Footer />
       </div>
     );
